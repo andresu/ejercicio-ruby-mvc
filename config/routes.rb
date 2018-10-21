4e63b2c70230c64ef19ea4fd16d4d01e97ec8249
Adnresu <andres.urreao@gmail.com>
2018-10-20 21:17:42 -0500

    Create comments box and functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'articles/new'
-  resources :articles
+  resources :articles do
+    resources :comments 
+  end
   get 'welcome/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root :to => "welcome#index"
